c6d7e000760d37db131becb4bdf58e94f42312a2
iKiray <exkiray@gmail.com>
2025-11-07 03:20:05 -0500

    Added variables and the main function 1. Added constants for random number, decimal, parsing 2. Added the main if function to decide which string to return 3. Added console log test and make sure it randomly printed the correct string

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+//variables
+const ranNum = Math.random() * 10;
+const fixNum = ranNum.toFixed(0);
+const parseNum = parseFloat(fixNum);
+
+//functions
+function getComputerChoice() {
+    if (parseNum <= 3) {
+        return "rock";
+    } else if (parseNum > 3 && parseNum <= 6) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
+
+console.log(getComputerChoice());
+
