ad4304d94d8620f002ea0306edf581fb0a6836bc
iKiray <exkiray@gmail.com>
2025-11-07 03:56:42 -0500

    Added prompt for human choice with console log print result

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
-//variables
 const ranNum = Math.random() * 10;
 const fixNum = ranNum.toFixed(0);
 const parseNum = parseFloat(fixNum);
 
-//functions
+
 function getComputerChoice() {
     if (parseNum <= 3) {
         return "rock";
@@ -14,5 +13,15 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+let choice = prompt("Enter: rock, paper, or scisscor")
+if (choice == "rock") {
+    console.log("your choice is rock");
+} else if (choice == "paper") {
+    console.log("your choice is paper");
+} else if (choice == "scissor") {
+    console.log("your choice is scissor");
+} else {
+    console.log("your choice is invalid");
+}
 
+console.log("computer choice: " + getComputerChoice());
