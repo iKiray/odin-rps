88274e573252b9e0fe674d03d841cf034d2638c7
iKiray <exkiray@gmail.com>
2025-11-07 05:02:51 -0500

    Added case insensitive to human choice function

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,9 @@ const ranNum = Math.random() * 10;
 const fixNum = ranNum.toFixed(0);
 const parseNum = parseFloat(fixNum);
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     if (parseNum <= 3) {
         return "rock";
@@ -12,16 +15,24 @@ function getComputerChoice() {
     }
 }
 
+console.log("computer choice: " + getComputerChoice());
+
 function getHumanChoice() {
     let choice = prompt("Enter: rock, paper, or scissor");
-    if (choice == "rock") {
+    let fixedChoice = choice.toLowerCase();
+
+    if (fixedChoice === "rock") {
         return "rock";
-    } else if (choice == "paper") {
+    } else if (fixedChoice === "paper") {
         return "paper";
     } else {
         return "scissor";
     }
 }
 
-console.log("computer choice: " + getComputerChoice());
 console.log("your choice: " + getHumanChoice());
+/*
+function playRound(humanChoice, computerChoice) {
+    
+}
+*/
